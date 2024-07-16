cfca224f2d3e08e3bd328dd3041095a422ca0c02
Winston Cooke <git@winstoncooke.com>
2024-07-16 12:19:51 -0400

    setup cpu

---
+++ README.md
@@ -0,0 +1,6 @@
+Following along these resources:
+
+- https://github.com/aquova/chip8-book?tab=readme-ov-file
+- https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
+- https://austinmorlan.com/posts/chip8_emulator/
+
---
+++ chip8_core/src/lib.rs
@@ -0,0 +1,81 @@
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+/// The Chip8’s memory from 0x000 to 0x1FF is reserved, so the ROM instructions must start at
+/// 0x200.
+const START_ADDRESS: u16 = 0x200;
+const FONTSET_START_ADDRESS: usize = 0x50;
+const FONTSET_SIZE: usize = 80;
+static FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+#[derive(Debug)]
+pub struct Chip8 {
+    /// Program Counter
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
+    i_reg: u16,
+    /// Stack Pointer
+    sp: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    /// Delay Timer
+    dt: u8,
+    /// Sound Timer
+    st: u8,
+}
+
+impl Chip8 {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDRESS,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        }
+    }
+}
+
+impl Default for Chip8 {
+    fn default() -> Self {
+        Self {
+            pc: START_ADDRESS,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        }
+    }
+}
