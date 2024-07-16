962f90b885d6dd83a6b9bd781edc08688d65dbae
Winston Cooke <git@winstoncooke.com>
2024-07-16 12:43:19 -0400

    add emulation methods

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -7,9 +7,9 @@ const NUM_KEYS: usize = 16;
 /// The Chip8’s memory from 0x000 to 0x1FF is reserved, so the ROM instructions must start at
 /// 0x200.
 const START_ADDRESS: u16 = 0x200;
-const FONTSET_START_ADDRESS: usize = 0x50;
+const FONTSET_START_ADDRESS: u16 = 0x50;
 const FONTSET_SIZE: usize = 80;
-static FONTSET: [u8; FONTSET_SIZE] = [
+const FONTSET: [u8; FONTSET_SIZE] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
     0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -48,7 +48,7 @@ pub struct Chip8 {
 
 impl Chip8 {
     pub fn new() -> Self {
-        Self {
+        let mut new_chip8 = Self {
             pc: START_ADDRESS,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
@@ -59,7 +59,66 @@ impl Chip8 {
             keys: [false; NUM_KEYS],
             dt: 0,
             st: 0,
+        };
+
+        new_chip8.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_chip8
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDRESS;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0
+    }
+
+    pub fn tick(&mut self) {
+        let _op = self.fetch();
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
         }
+
+        if self.st > 0 {
+            // todo: implement audio
+            if self.st == 1 {
+                // BEEP
+            }
+
+            self.st -= 1
+        }
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+
+        op
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        if self.stack.is_empty() {
+            panic!("Stack is empty");
+        }
+
+        self.sp -= 1;
+        self.stack[self.sp as usize]
     }
 }
 
